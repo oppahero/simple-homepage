41cebe9032a25a256125d2915d82e329085123a2
María López <37983313+oppahero@users.noreply.github.com>
2023-10-18 13:52:04 -0400

    Toogle menu for mobile and tablet

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const body = document.querySelector("body");
+const nav = document.querySelector(".nav__ul");
+const menu = document.querySelector(".menu");
+const close = document.querySelector(".close");
 const toggleButton = document.querySelector(".mode");
 const logo = document.querySelector(".logo img");
 const moon = document.querySelector("#moon");
@@ -6,17 +9,25 @@ const sun = document.querySelector("#sun");
 
 const sunMode = () => {
   sun.className = "mode--activate";
-  moon.className = null;
-  body.className = null;
+  moon.className = "";
+  body.className = "";
 };
 
 const moonMode = () => {
-  body.className = "dark"
+  body.className = "dark";
   moon.className = "mode--activate";
-  sun.className = null;
+  sun.className = "";
 };
 
-// toogleMode();
-// toggleButton.addEventListener("click", toogleMode);
+const showNav = () => {
+    nav.style.transform = "translateX(0px)";
+};
+
+const closeNav = () => {
+  nav.style.transform = "translateX(424px)";
+};
+
+menu.addEventListener("click", showNav);
+close.addEventListener("click", closeNav);
 sun.addEventListener("click", sunMode);
 moon.addEventListener("click", moonMode);
