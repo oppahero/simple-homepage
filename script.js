05ba7399c91c5a28bc4ea1758816b5b593b51629
María López <37983313+oppahero@users.noreply.github.com>
2023-10-18 12:03:35 -0400

    Mobile firts. Queries for mobile, tablet and desktop

--- script.js
+++ script.js
@@ -4,21 +4,15 @@ const logo = document.querySelector(".logo img");
 const moon = document.querySelector("#moon");
 const sun = document.querySelector("#sun");
 
-// const toogleMode = () => {
-//   logo.src = body.classList.contains("dark")
-//     ? "./alarado-icon-homepage-dark-mode.svg"
-//     : "./assets/alarado-icon-homepage.svg";
-// };
-
 const sunMode = () => {
-  sun.className = "button-icon";
+  sun.className = "mode--activate";
   moon.className = null;
   body.className = null;
 };
 
 const moonMode = () => {
   body.className = "dark"
-  moon.className = "button-icon";
+  moon.className = "mode--activate";
   sun.className = null;
 };
 
